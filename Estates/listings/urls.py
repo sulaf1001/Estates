f46f2ca90a01be709b84e31450e5989c6a59a795
sulaf1001 <sulafali1001@gmail.com>
2025-06-28 22:58:21 +0000

    Latest changes

--- Estates/listings/urls.py
+++ Estates/listings/urls.py
@@ -9,10 +9,10 @@ path('delete_property/<int:pk>/', views.DeleteProperty.as_view(), name='Delete')
 path('update_property/<int:pk>/', views.UpdateProperty.as_view(), name='Update'),
 path('properties/<int:pk>/', views.PropertyDetails.as_view(), name='property_details' ),
 path('mylistings/',views.MyListings.as_view(),name='MyListings'),
-path('rent/', views.RentPropertiesView.as_view()),
-path('buy/', views.BuyPropertiesView.as_view()),
+# path('rent/', views.RentPropertiesView.as_view()),
+# path('buy/', views.BuyPropertiesView.as_view()),
 path('properties/<int:pk>/favourites/', views.FavouritesView.as_view(), name='favourites'),
 path('myfavourites/', views.MyFavouritesView.as_view(), name='myfavourites'),
 path('propertyFilter/', views.PropertyFilterListView.as_view(), name='cars-filter'),
-    
+
 ]
